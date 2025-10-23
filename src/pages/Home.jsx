19a379aac9a038e0a72685796d802c38dff9565a
Kavinkumar888 <kavi8668182885@gmail.com>
2025-10-23 10:34:41 +0530

    commit is changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
-import HomeCatalog from '../components/HomeCatalog'
-import About from '../components/About'
-import ContactUs from '../components/ContactUs'
+import React from "react";
+import HomeCatalog from "../components/HomeCatalog";
+import About from "../components/About";
+import ContactUs from "../components/ContactUs";
 
 const Home = () => {
   return (
-    <div>
+    <div className="bg-gray-50 min-h-screen">
+      {/* Hero / Catalog Section */}
+      <section className="pt-8">
         <HomeCatalog />
-        <About />
-        <ContactUs />
-    </div>
-  )
-}
+      </section>
 
-export default Home
+      {/* About Section */}
+      <section className="py-12 px-4 md:px-8 bg-white shadow-sm rounded-2xl my-8 mx-2 md:mx-12">
+        <About />
+      </section>
+
+      {/* Contact Section */}
+      <section className="pb-12 px-4 md:px-8">
+        <ContactUs />
+      </section>
+    </div>
+  );
+};
+
+export default Home;
--- src/pages/TshirtPage.jsx
+++ src/pages/TshirtPage.jsx
@@ -159,18 +159,6 @@ const TshirtPage = () => {
           </div>
         </div>
       )}
-
-      {/* Notification */}
-      {notification && (
-        <div className="fixed top-24 right-6 bg-green-500 text-white px-6 py-3 rounded-lg shadow-lg transform transition-all duration-500 ease-in-out z-40">
-          <div className="flex items-center space-x-2">
-            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-              <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-            </svg>
-            <span>{notification}</span>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
